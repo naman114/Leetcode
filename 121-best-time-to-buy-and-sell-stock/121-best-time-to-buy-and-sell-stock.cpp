fc553d0351f60dd74c5ae037cecb777206c5a2aa
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-10 14:11:55 +0530

    Time: 147 ms (73.30%), Space: 93.4 MB (53.73%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int min_so_far = INT_MAX;
+        int max_profit = 0;
+        
+        for(int price: prices){
+            min_so_far = min(min_so_far, price); // lowest buying price so far
+            max_profit = max(max_profit, price - min_so_far); // take max of profits earned by theoretically selling everyday
+        }
+        
+        return max_profit;
+    }
+};
+
+/*
+
+V2: We are are selling everyday here based on the lowest price so far
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_so_far = INT_MAX;
         int max_after_lowest = INT_MIN;
         int max_profit = 0;
         
@@ -21,7 +40,11 @@ public:
     }
 };
 
+*/
+
 /*
+V1: We need global best profit. This method gives the 'last' local best profit 
+WA on [2,4,1]
 
 class Solution {
 public:
@@ -41,7 +64,5 @@ public:
         return max_after_lowest == INT_MIN ? 0 : max_after_lowest - min_so_far;
     }
 };
-
-WA on [2,4,1]
 
 */
