22b6d30120f21d7409fe462e90316d66f93c2df5
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-11 00:29:22 +0530

    Time: 25 ms (75.28%), Space: 19.2 MB (83.81%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,18 +1,16 @@
+// TC: O(N)
+// SC: O(1)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int idx = 0; // idx to place a non zero element
-        int cnt = 0; // cnt of zeros
         for(int i = 0; i < (int)nums.size(); ++i){
             if(nums[i] != 0)
                 nums[idx++] = nums[i];
-            else
-                cnt++;
         }
         
-        idx = nums.size() - 1;
-        while(cnt--){
-            nums[idx--] = 0;
+        while(idx < (int)nums.size()){
+            nums[idx++] = 0;
         }
     }
 };
