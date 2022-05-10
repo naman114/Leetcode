a8834e13d18113322cf76d8736bb5fa000d8fc28
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-11 02:41:09 +0530

    Time: 41 ms (29.16%), Space: 31 MB (85.58%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,5 +1,26 @@
 // TC: O(N)
 // SC: O(1)
+
+// V2
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < n; ++i) sum += nums[i];
+        
+        int sumL = 0;
+        for(int i = 0; i < n; ++i){
+            if(sumL == sum - sumL - nums[i])
+                return i;          
+            sumL += nums[i];
+        }
+        
+        return -1;
+    }
+};
+
+/* V1: With right sum
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
@@ -21,3 +42,4 @@ public:
         return -1;
     }
 };
+*/
