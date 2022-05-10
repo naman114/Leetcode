58567fad48189ea4b1b8c6ae79db88e05d713b58
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-10 16:05:45 +0530

    Time: 7 ms (21.42%), Space: 9 MB (90.32%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,3 +1,31 @@
+// TC: O(m + n)
+// SC: O(1)
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]) 
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
+        
+        while(i >= 0)
+            nums1[k--] = nums1[i--];
+        
+        while(j >= 0)
+            nums1[k--] = nums2[j--];
+    }
+};
+
+/*
+TC: O(m + n)
+SC: O(m + n)
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -36,3 +64,5 @@ public:
         for(int i = m; i < m + n; ++i) nums1[i] = ans[i];
     }
 };
+
+*/
