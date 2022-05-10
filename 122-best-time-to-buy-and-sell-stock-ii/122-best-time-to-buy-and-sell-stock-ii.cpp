07bbbb9fbf2d8f5a46ce2dc03f75f37a0ad69aa3
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-11 01:52:39 +0530

    Time: 7 ms (75.69%), Space: 13 MB (88.12%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -11,9 +11,11 @@ public:
         for(int i = 1; i < (int) prices.size(); ++i){
             // Every day:
             
-            // If you are in the buying mood, you can either buy that day by cutting that day's price from your profits on selling the day before or you continue your last purchase
+            // If you are in the buying mood, you can either buy that day by cutting that day's price from your profits on selling the day before 
+            // or you continue your last purchase
             int temp_buy = max(sell - prices[i], buy);
-            // If you are in the selling mood, you can either sell that day by adding that day's price to your profits on buying the day before or you didn't buy any new stock and just maintain your profits after the last sale 
+            // If you are in the selling mood, you can either sell that day by adding that day's price to your profits on buying the day before 
+            // or you didn't buy any new stock and just maintain your profits after the last sale 
             int temp_sell = max(buy + prices[i], sell);
             
             buy = temp_buy;
