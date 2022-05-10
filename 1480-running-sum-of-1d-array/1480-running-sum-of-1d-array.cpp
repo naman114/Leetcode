7f6baebeb546d0e3929669cb5daa8f7d3867aeed
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-11 02:05:26 +0530

    Time: 8 ms (19.84%), Space: 8.6 MB (38.08%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,11 @@
+// TC: O(N)
+// SC: O(1)
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i = 1; i < (int) nums.size(); ++i)
+            nums[i] += nums[i - 1];
+        return nums;
+    }
+};
