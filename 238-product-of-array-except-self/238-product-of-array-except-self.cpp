7238a7b89f1630d4cbf6f2a062835c0259af18b7
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-12 01:41:54 +0530

    Time: 38 ms (34.03%), Space: 23.9 MB (81.62%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,3 +1,30 @@
+//TC: O(N)
+//SC: O(1)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(), 1);
+        
+        int pL = 1;
+        
+        for(int i = 0; i < nums.size() - 1; ++i){
+            pL *= nums[i];
+            ans[i + 1] *= pL;
+        }
+        
+        int pR = 1;
+        
+        for(int i = nums.size() - 1; i >= 1; --i){
+            pR *= nums[i];
+            ans[i - 1] *= pR;
+        }
+        
+        return ans;
+    }
+};
+
+
+/*
 // TC: O(N)
 // SC: O(N)
 class Solution {
@@ -26,3 +53,4 @@ public:
         return ans;
     }
 };
+*/
