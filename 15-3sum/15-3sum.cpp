aa74f94e345e9de3b366c3d146503553f2a2b9b5
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-12 01:10:53 +0530

    Time: 88 ms (80.83%), Space: 25 MB (19.43%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> twoSum(vector<int> &nums, int target, int start){
+        int lo = start, high = nums.size() - 1;
+        vector<vector<int>> ans;
+        
+        while(lo < high){
+            int sum = nums[lo] + nums[high];
+            if(sum == target){
+                ans.push_back({nums[lo], nums[high]});
+                while(lo < high && nums[lo] == nums[lo + 1]) lo++;
+                while(lo < high && nums[high] == nums[high - 1]) high--;
+                lo++;
+                high--;
+            }
+            else if(sum < target)
+                lo++;
+            else
+                high--;
+        }
+        
+        return ans;
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        
+        for(int i = 0; i < nums.size(); ++i){
+            cout<<i<<endl;
+            vector<vector<int>> res = twoSum(nums, -nums[i], i + 1);
+            if(res.size()){
+                for(auto v: res)
+                    ans.push_back({nums[i], v[0], v[1]});
+            }
+            while(i < nums.size() - 1 && nums[i] == nums[i + 1]) i++;
+        }
+        
+        return ans;
+    }
+};
