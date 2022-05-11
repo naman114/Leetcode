3289e60c6dce93380e90e2453bdcbd0a315dbec3
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-11 23:32:10 +0530

    Time: 51 ms (48.87%), Space: 19.1 MB (67.41%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,5 +1,26 @@
 // TC: O(NlogN)
 // SC: O(N)
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& itv) {
+        sort(itv.begin(), itv.end());
+        
+        vector<vector<int>> ans;
+        
+        ans.push_back(itv[0]);
+        
+        for(int i = 1; i < itv.size(); ++i){
+            if(itv[i][0] <= ans.back()[1])
+                ans.back()[1] = max(ans.back()[1], itv[i][1]);
+            else
+                ans.push_back(itv[i]);
+        }
+        return ans;
+    }
+};
+
+/* V1: Stack
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& itv) {
@@ -39,3 +60,4 @@ public:
         return ans;
     }
 };
+*/
