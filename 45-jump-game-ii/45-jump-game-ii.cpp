71221f9d264ef37fa3fc1e75dd2fcd7b4f49ff61
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-13 01:13:27 +0530

    Time: 403 ms (28.03%), Space: 17.2 MB (34.89%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX); // dp[i] means min moves to reach last idx
+        dp[n - 1] = 0;
+        
+        for(int i = n - 2; i >= 0; --i){
+            int mn = INT_MAX;
+            for(int j = 1; j <= nums[i] && i + j < n; ++j){
+                mn = min(mn, dp[i + j]);
+            }
+            if(mn != INT_MAX)
+                dp[i] = 1 + mn;
+        }
+        
+        return dp[0]; // since a solution always exists
+    }
+};
