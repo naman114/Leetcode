791da531f7f3309d25640fed00f8ac8f0de33827
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-05-14 04:09:46 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (39.69%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> solve(vector<int>& nums, int idx) {
+        vector<vector<int>> ans;
+        
+        if(idx == nums.size()){
+            ans.push_back({});
+            return ans;
+        }
+
+        // Give me the subsets of the remaining array
+        vector<vector<int>> inc = solve(nums, idx + 1);
+        for(auto v: inc){
+            // exclude nums[idx]
+            ans.push_back(v);
+            // include nums[idx]
+            v.push_back(nums[idx]);
+            ans.push_back(v);
+        }
+        return ans;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        return solve(nums, 0);
+    }
+};
