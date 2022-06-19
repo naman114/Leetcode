c82caa8fd8a84116bbad8778c04ebe6a71c3ea4e
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-06-19 18:22:46 +0530

    Time: 29 ms (25.09%), Space: 20.6 MB (6.88%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,25 @@
+/*
+1. TC: O(N^2), SC: O(1) when for every idx you check for the max on the left and right
+
+2. Prefix arrays
+TC: O(N)
+SC: O(N)
+*/
 class Solution {
 public:
     int trap(vector<int>& h) {
         int n = h.size();
         vector<int> lm(n), rm(n);
         
-        lm[0] = 0;
-        for(int i = 1; i < h.size(); ++i){
-            if(h[lm[i - 1]] > h[i])
-                lm[i] = lm[i - 1];
-            else
-                lm[i] = i;
-        }
+        lm[0] = h[0];
+        for(int i = 1; i < h.size(); ++i) lm[i] = max(lm[i - 1], h[i]);
         
-        rm[n - 1] = n - 1;
-        for(int i = n - 2; i >= 0; --i){
-            if(h[rm[i + 1]] > h[i])
-                rm[i] = rm[i + 1];
-            else
-                rm[i] = i;
-        }
+        rm[n - 1] = h[n - 1];
+        for(int i = n - 2; i >= 0; --i) rm[i] = max(rm[i + 1], h[i]);
         
         int ans = 0;
         
-        for(int i = 0; i < n; ++i){
-            if(lm[i] == i || rm[i] == i)
-                continue;
-            
-            ans += min(h[lm[i]], h[rm[i]]) - h[i];
-        }
+        for(int i = 0; i < n; ++i) ans += min(lm[i], rm[i]) - h[i];
         
         return ans;
     }
