11419dd099af21732e2df8c6d023a68d72786bba
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-06-19 17:55:39 +0530

    Time: 8 ms (75.38%), Space: 8 MB (80.46%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,5 +1,39 @@
 class Solution {
     unordered_set<string> us;
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.length();
+        us.clear();
+        
+        for(string st: wordDict) us.insert(st);
+        
+        vector<int> dp(n + 1);
+        dp[n] = 1;
+        
+        for(int i = n - 1; i >= 0; --i){
+            
+            string cur_string = "";
+            
+            for(int j = i; j < n; ++j){
+                cur_string += s[j];
+                
+                if(us.count(cur_string)){
+                    if(dp[j + 1]){dp[i] = 1; break;}
+                }   
+            }
+    
+        }
+        
+        
+        return dp[0];
+    }
+};
+
+/*
+Top Down
+
+class Solution {
+    unordered_set<string> us;
     vector<int> dp;
 public:
     bool solve(string &s, int start){
@@ -29,3 +63,5 @@ public:
         return solve(s, 0);
     }
 };
+
+*/
