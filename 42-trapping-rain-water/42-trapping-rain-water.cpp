cb41b9469ec0eda54fe67992800312bcbfdf14af
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-06-19 19:41:45 +0530

    Time: 28 ms (28.16%), Space: 19.6 MB (60.76%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,10 +1,10 @@
 /*
 1. TC: O(N^2), SC: O(1) when for every idx you check for the max on the left and right
 
-2. Prefix arrays
+2. Prefix max and suffix max arrays
 TC: O(N)
 SC: O(N)
-*/
+
 class Solution {
 public:
     int trap(vector<int>& h) {
@@ -20,6 +20,40 @@ public:
         int ans = 0;
         
         for(int i = 0; i < n; ++i) ans += min(lm[i], rm[i]) - h[i];
+        
+        return ans;
+    }
+};
+
+*/
+
+/*
+3. 2 Pointers (Most optimal)
+TC: O(N)
+SC: O(1) 
+*/
+
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+        
+        int l = 0, r = n - 1;
+        int LMaxHeight = 0, RMaxHeight = 0;
+        int ans = 0;
+        
+        while(l <= r){
+            if(h[l] <= h[r]){
+                if(h[l] >= LMaxHeight) LMaxHeight = h[l];
+                else ans += LMaxHeight - h[l];
+                l++;
+            }
+            else{
+                if(h[r] >= RMaxHeight) RMaxHeight = h[r];
+                else ans += RMaxHeight - h[r];
+                r--;
+            }
+        }
         
         return ans;
     }
