464a0fda812618f8704b8f32a3ea1c8d9c169ccd
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-06-19 16:04:35 +0530

    Time: 4 ms (22.74%), Space: 6.3 MB (14.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,3 +1,30 @@
+/*
+Recurrence Relation
+dp[i] = dp[i - 1] + dp[i - 2]
+i == 0 return 1;
+i < 0 return 0;
+*/
+class Solution {
+public:
+    int solve(int n, vector<int> &dp){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        dp[0] = 1;
+        
+        return solve(n, dp);
+    }
+};
+
+/*
+Permutations
+TC: O(N)
+SC: O(N)
+
 class Solution {
 public:
     int climbStairs(int n) {
@@ -16,3 +43,5 @@ public:
         return dp[n];
     }
 };
+
+*/
