a3191ab2ff89de2cf2a822143c8efba0ca66c58c
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-06-21 03:13:39 +0530

    Time: 11 ms (35.11%), Space: 7.7 MB (87.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,7 +1,20 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 0; // dp[i] means number of set bits in i
+        
+        for(int i = 1; i <= n; ++i)
+            dp[i] = dp[i>>1] + (i&1);
+            
+        return dp;
+    }
+};
+
 /*
 TC: O(NlogN)
 SC: O(N)
-*/
+
 class Solution {
 public:
     int count(int n){
@@ -20,3 +33,4 @@ public:
         return ans;
     }
 };
+*/
