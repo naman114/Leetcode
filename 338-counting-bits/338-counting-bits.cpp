b8fd027b467905f2e4499cc36114434717de2fbe
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-06-21 02:59:30 +0530

    Time: 14 ms (16.89%), Space: 7.9 MB (73.10%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,22 @@
+/*
+TC: O(NlogN)
+SC: O(N)
+*/
+class Solution {
+public:
+    int count(int n){
+        int cnt = 0;
+        while(n){
+            cnt += n&1;
+            n >>= 1;
+        }
+        return cnt;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1);
+        for(int i = 0; i <= n; ++i){
+            ans[i] = count(i);
+        }
+        return ans;
+    }
+};
