8efea25eb846a2eea350db8357751de90ec93f36
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-07-01 12:38:40 +0530

    Time: 35 ms (98.65%), Space: 16 MB (77.60%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    static bool compare(vector<int> &a, vector<int> &b){
+        return a[1] > b[1];
+    }
+    
+     int maximumUnits(vector<vector<int>>& items, int wt) {
+         sort(items.begin(), items.end(), compare);
+         int ans = 0;
+         
+         for(int i = 0; i < items.size(); ++i){
+             if(wt <= 0) break;
+             if(wt - items[i][0] >= 0){
+                 ans += items[i][0] * items[i][1];
+                 wt -= items[i][0];
+             }
+             else{
+                 ans += wt * items[i][1];
+                 wt = 0;
+             }
+         }
+         
+         return ans;
+    }
+    
+/*
+2,5
+3,9
+4,7
+5,10
+
+10 + 27 + 28 + 10 
+1
+*/
+/*
+    int maximumUnits(vector<vector<int>>& items, int wt) {
+        int n = items.size();
+        vector<vector<int>> dp(n + 1, vector<int>(wt + 1));
+        
+        // dp[i][j] means max value upto i items and j knapsack size
+        
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= wt; ++j){
+                for(int k = 1; k <= items[i - 1][0]; ++k){
+                    if(j - k >= 0)
+                        dp[i][j] = max({dp[i][j], dp[i - 1][j], dp[i - 1][j - k] + k*items[i - 1][1]});
+                    else{
+                        dp[i][j] = dp[i - 1][j];
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return dp[n][wt];
+    }
+*/
+};
