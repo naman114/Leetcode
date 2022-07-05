74f8f9c03a61ef638323130d071132ea5e6a502e
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-07-05 17:01:57 +0530

    Time: 844 ms (70.58%), Space: 286 MB (15.06%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.cpp
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cpp
@@ -1,17 +1,16 @@
 class RangeFreqQuery {
-    unordered_map<int, vector<int>> ump;
+    vector<vector<int>> v;
 public:
     RangeFreqQuery(vector<int>& arr) {
-        for(int i = 0; i < arr.size(); ++i) ump[arr[i]].push_back(i);
+        v.resize(100001);
+        for(int i = 0; i < arr.size(); ++i) v[arr[i]].push_back(i);
     }
     
     int query(int left, int right, int value) {
-        if(!ump.count(value)) return 0;
-        
         vector<int>::iterator it1, it2;
         
-        it1 = lower_bound(ump[value].begin(), ump[value].end(), left);       
-        it2 = upper_bound(ump[value].begin(), ump[value].end(), right);
+        it1 = lower_bound(v[value].begin(), v[value].end(), left);       
+        it2 = upper_bound(v[value].begin(), v[value].end(), right);
         
         return it2 - it1;
     }
