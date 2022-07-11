d7b5ba1d56de10f79c4edce6108d686500a269aa
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-07-11 10:38:50 +0530

    Time: 7 ms (46.19%), Space: 12.1 MB (19.97%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -19,6 +19,35 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         
+        while(q.size()){
+            int n = q.size();
+            for(int i = 0; i < n; ++i){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if(i == n-1) ans.push_back(node->val);
+                
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        
+        if(!root) return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
         TreeNode* last = root;
         
         while(q.size()){
@@ -41,3 +70,4 @@ public:
         return ans;
     }
 };
+*/
