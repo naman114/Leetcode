34a5016127d193611edf2f717f406e2eb7782dcc
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-07-28 23:30:04 +0530

    Time: 84 ms (5.02%), Space: 32.4 MB (5.00%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> solve(string ans, int osf, int csf, int n){
+        vector<string> res;
+        
+        if(ans.length() == 2*n){
+            if(osf == csf)
+                res.push_back(ans);
+            return res;
+        }
+        
+        vector<string> open = solve(ans + '(', osf + 1, csf, n);
+        
+        for(string s: open) res.push_back(s);
+        
+        if(osf > csf){
+            vector<string> closed = solve(ans + ')', osf, csf + 1, n);
+            for(string s: closed) res.push_back(s);
+        }
+        
+        return res;
+    }
+    vector<string> generateParenthesis(int n) {
+        return solve("", 0, 0, n);
+    }
+};
