8768d90314a819d264903ac8435f556c49c8803c
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-08-05 03:25:15 +0530

    Time: 86 ms (33.33%), Space: 42 MB (66.67%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} p
+ * @param {number} q
+ * @return {number}
+ */
+var mirrorReflection = function(p, q) { 
+    if (q === 0) return 0;
+    
+    let height = q;
+    while (height < p || height % p) {
+        height += q;
+    }
+    
+    const upFlip = height / p;
+    const rightFlip = height / q;
+
+    const topCorner = upFlip % 2 ? 1 : 0;
+    
+    return rightFlip % 2 ? topCorner : 2;
+};
