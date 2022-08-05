1340a894482e751ec95087e3af36a0c165692d1f
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-08-06 00:44:28 +0530

    Time: 4 ms (61.91%), Space: 11.8 MB (99.12%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -9,6 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        
+        if(!root) return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int dir = 1; // LR 1, RL 0
+        
+        while(!q.empty()){
+            int n = q.size();
+            
+            vector<int> lvl(n);
+            
+            for(int i = 0; i < n; ++i){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if(dir == 1) lvl[i] = node->val;
+                else lvl[n - i - 1] = node->val;
+                
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            
+            ans.push_back(lvl);
+            
+            dir ^= 1;
+        }
+        
+        return ans;
+    }
+};
+
+/*
+Reversing the vector
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -47,3 +88,5 @@ public:
         return ans;
     }
 };
+
+*/
