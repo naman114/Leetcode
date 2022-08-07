29c001ab53f93261443671a75433745de76572f9
Naman Gogia <57593654+naman114@users.noreply.github.com>
2022-08-08 00:28:14 +0530

    Time: 320 ms (14.02%), Space: 160.6 MB (5.29%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    vector<vector<int>> dp;
+    int mod = 1e9 + 7;
+public:
+    int solve(int n, char prev, unordered_map<char, vector<char>> &ump){
+        if(n == 0) return 1;
+        
+        if(dp[n][prev] != -1) return dp[n][prev];
+        
+        int ans = 0;
+        
+        for(char c: ump[prev]){
+            ans = (ans%mod + solve(n - 1, c, ump)%mod)%mod;
+        }
+        
+        return dp[n][prev] = ans;
+    }
+    int countVowelPermutation(int n) {
+        dp.resize(n, vector<int>(123, -1));
+        
+        unordered_map<char, vector<char>> ump;
+        ump['a'] = {'e'};
+        ump['e'] = {'a', 'i'};
+        ump['i'] = {'a', 'e', 'o', 'u'};
+        ump['o'] = {'i', 'u'};
+        ump['u'] = {'a'};
+        
+        int ans = 0;
+        
+        for(auto p: ump)
+            ans = (ans%mod + solve(n - 1, p.first, ump)%mod)%mod;
+        
+        return ans;
+    }
+};
